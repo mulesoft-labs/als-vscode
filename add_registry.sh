f23da76fae43190ec4e6ec07dc92ece7fb21b55b
llibarona <llibarona@salesforce.com>
2021-01-27 13:24:33 -0300

    wip

--- add_registry.sh
+++ add_registry.sh
@@ -1,12 +1,14 @@
 echo "Connecting to MuleSoft internal registry"
 printf "@mulesoft:registry=https://nexus3.build.msap.io/repository/npm-internal/\n//nexus3.build.msap.io/repository/npm-internal/:_authToken=$NPM_TOKEN" > .npmrc
 touch .npmrc
-echo "npm_package_engines_vscode='^1.19.0' node ./node_modules/vscode/bin/install"
-npm_package_engines_vscode="^1.19.0" node ./node_modules/vscode/bin/install
 echo "npm --version"
 npm --version
 echo "node --version"
 node --version
+echo "npm ci"
+npm ci
+echo "npm_package_engines_vscode='^1.19.0' node ./node_modules/vscode/bin/install"
+npm_package_engines_vscode="^1.19.0" node ./node_modules/vscode/bin/install
 echo "npm ci"
 npm ci
 echo "ls -larth node_modules"
