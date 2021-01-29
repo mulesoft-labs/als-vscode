e5b7a0c1b9689762e612e4c4cf449e2298f33ab6
llibarona <llibarona@salesforce.com>
2021-01-29 10:42:45 -0300

    wip

--- add_registry.sh
+++ add_registry.sh
@@ -1,6 +1,11 @@
 echo "Connecting to MuleSoft internal registry"
 printf "@mulesoft:registry=https://nexus3.build.msap.io/repository/npm-internal/\n//nexus3.build.msap.io/repository/npm-internal/:_authToken=$NPM_TOKEN" > .npmrc
 touch .npmrc
+echo "sed -i 's/{VERSION}/'$VERSION'/' package.json"
+sed -i 's/{VERSION}/'$VERSION'/' package.json
+echo "sed -i 's/{VERSION}/'$VERSION'/' package-lock.json"
+sed -i 's/{VERSION}/'$VERSION'/' package-lock.json
+npm --version
 echo "npm --version"
 npm --version
 echo "node --version"
