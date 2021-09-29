68b6951cb97e591a6174bbd64ff20817cf41ffb3
llibarona <llibarona@salesforce.com>
2021-09-29 16:58:35 -0300

    extend alsLog to handler and fix predicate for unregister

--- src/server/handlers.ts
+++ src/server/handlers.ts
@@ -2,19 +2,13 @@ import { AlsLanguageClient } from "./als"
 import * as vscode from 'vscode';
 import { DidChangeConfigurationNotificationParams, DependencyConfiguration, isDependencyConfiguration } from "../types";
 import { awaitInputBox } from "../ui/ui";
+import {alsLog} from "../extension"
 
 export const registerProfileHandler = (als: AlsLanguageClient) => {
   return (fileUri: vscode.Uri) => {
-    console.log("Registering profile ", fileUri)
+    alsLog.appendLine("Registering profile " + fileUri)
     const uri = als.languageClient.code2ProtocolConverter.asUri(fileUri)
     als.getWorkspaceConfiguration(uri).then(workspaceConfig => {
-      // const profiles: DependencyConfiguration[] = workspaceConfig.configuration
-      //   .dependencies
-      //   .filter(isDependencyConfiguration)
-      //   .filter(v => {
-      //     v.scope == "custom-validation" && v.file.toLowerCase != uri.toLowerCase
-      //   })
-      // profiles.push({file: uri, scope: "custom-validation"});
       const newWorkspaceConfig: DidChangeConfigurationNotificationParams = {
         mainUri: workspaceConfig.configuration.mainUri,
         folder: uri,
@@ -27,14 +21,15 @@ export const registerProfileHandler = (als: AlsLanguageClient) => {
 
 export const unregisterProfileHandler = (als: AlsLanguageClient) => {
   return (fileUri: vscode.Uri) => {
-    console.log("Unregistering profile ", fileUri)
+    alsLog.appendLine("Unregistering profile " + fileUri)
     const uri = als.languageClient.code2ProtocolConverter.asUri(fileUri)
     als.getWorkspaceConfiguration(uri).then(workspaceConfig => {
       const newWorkspaceConfig: DidChangeConfigurationNotificationParams = {
         mainUri: workspaceConfig.configuration.mainUri,
         folder: uri,
-        dependencies: workspaceConfig.configuration.dependencies.filter(v => {
-          !isDependencyConfiguration(v) || !(v.scope == "custom-validation" && v.file.toLowerCase != uri.toLowerCase)
+        dependencies: workspaceConfig.configuration.dependencies
+        .filter(v => {
+          !isDependencyConfiguration(v) || !(v.scope == "custom-validation" && v.file.toLowerCase() == uri.toLowerCase())
         })
       }
       als.changeWorkspaceConfigurationCommand(newWorkspaceConfig);
@@ -45,27 +40,27 @@ export const unregisterProfileHandler = (als: AlsLanguageClient) => {
 
 export const serializationHandler = (als: AlsLanguageClient) => {
   return (fileUri: vscode.Uri) => {
-    console.log("als.serialization called")
+    alsLog.appendLine("als.serialization called")
     als.sendSerializationRequest(fileUri)
   }
 }
 
 export const renameFileHandler = (als: AlsLanguageClient) => {
   return (fileUri: vscode.Uri) => {
-    console.log("als.rename called")
+    alsLog.appendLine("als.rename called")
     var splittedPath = fileUri.toString().split("/")
     const oldFileName = splittedPath[splittedPath.length - 1]
     const splittedName = oldFileName.split(".")
     const currentExtension = splittedName[splittedName.length - 1]
     const originalPath = fileUri.toString().slice(0, fileUri.toString().lastIndexOf(oldFileName))
-    console.log("Old name: " + fileUri.toString() + " (" + oldFileName + ")")
+    alsLog.appendLine("Old name: " + fileUri.toString() + " (" + oldFileName + ")")
 
     awaitInputBox(oldFileName, "New name", "New file name", [0, oldFileName.lastIndexOf(currentExtension) - 1])
       .then((newName) => {
         if (newName === undefined) {
-          console.log("Rename cancelled")
+          alsLog.appendLine("Rename cancelled")
         } else {
-          console.log("New name: " + originalPath + newName)
+          alsLog.appendLine("New name: " + originalPath + newName)
           als.sendRenameRequest(fileUri, originalPath, newName);
         }
       })
@@ -74,29 +69,29 @@ export const renameFileHandler = (als: AlsLanguageClient) => {
 
 export const conversionHandler = (als: AlsLanguageClient) => {
   return (fileUri: vscode.Uri) => {
-    console.log("als.conversion called")
+    alsLog.appendLine("als.conversion called")
     var splittedPath = als.languageClient.code2ProtocolConverter.asUri(fileUri).split("/")
     const oldFileName = splittedPath[splittedPath.length - 1]
     const splittedName = oldFileName.split(".")
     const currentExtension = splittedName[splittedName.length - 1]
     const originalPath = als.languageClient.code2ProtocolConverter.asUri(fileUri).slice(0, als.languageClient.code2ProtocolConverter.asUri(fileUri).lastIndexOf(oldFileName))
-    console.log("Old name: " + als.languageClient.code2ProtocolConverter.asUri(fileUri) + " (" + oldFileName + ")")
+    alsLog.appendLine("Old name: " + als.languageClient.code2ProtocolConverter.asUri(fileUri) + " (" + oldFileName + ")")
 
 
     awaitInputBox(oldFileName, "New name", "New file name", [0, oldFileName.lastIndexOf(currentExtension) - 1])
       .then((newName) => {
         if (newName === undefined) {
-          console.log("Conversion cancelled")
+          alsLog.appendLine("Conversion cancelled")
         } else {
           const newUri = vscode.Uri.parse(originalPath + newName)
-          console.log("New conversion name: " + newUri)
+          alsLog.appendLine("New conversion name: " + newUri)
 
           awaitInputBox("AMF Graph", "Target Vendor", "New file Vendor Name")
             .then((vendor) => {
               if (vendor === undefined) {
-                console.log("Conversion cancelled")
+                alsLog.appendLine("Conversion cancelled")
               } else {
-                console.log("New conversion vendor: " + vendor)
+                alsLog.appendLine("New conversion vendor: " + vendor)
                 als.sendConversionRequest(fileUri, newUri, vendor);
               }
             })
